c859b4adf4455101fa10923690142d368edddc07
AntonAks <Freelancer1408>
2019-02-20 23:47:48 +0200

    replace Job class to Job application
    Co-Authored-By: Anton Aksonov <antonaks84@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-# TODO: need to read about models.Model module
-
-class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = model.CharField(max_length=200)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# TODO: need to read about models.Model module
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = model.CharField(max_length=200)
