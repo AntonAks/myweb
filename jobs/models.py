7f70d3e9860bd1f05d117d90d9c71a44f5f0ec95
AntonAks <Freelancer1408>
2019-02-20 23:58:14 +0200

    add migration and fix typo error.
    Co-Authored-By: Anton Aksonov <antonaks84@gmail.com>

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = model.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
