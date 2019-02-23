0d753f7731e4b67a923ecc5f4bc5f04f39782da6
AntonAks <Freelancer1408>
2019-02-23 15:03:21 +0200

    add blog preferences, change database
    Co-Authored-By: Anton Aksonov <antonaks84@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    
