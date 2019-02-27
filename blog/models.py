243f083e53078da8b8368b85b260de802a2f8285
AntonAks <antonaks84@gmail.com>
2019-02-28 00:12:46 +0200

    add something to Blog

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Blog(models.Model):
+    text_limit = 150
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField()
     body = models.TextField()
-    
+
+    def summary(self):
+        if len(self.body) > self.text_limit:
+            return self.body[:self.text_limit] + '...'
+        else:
+            return self.body
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home'),
+            path('', views.allblogs, name='home'),
 ]
