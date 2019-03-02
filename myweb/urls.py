5727c5632a3b6af6099eaf8b611b17fd496e5d8f
AntonAks <antonaks84@gmail.com>
2019-03-02 23:11:19 +0200

    experiments with bootstrap

--- myweb/urls.py
+++ myweb/urls.py
@@ -6,8 +6,8 @@ import jobs.views
 import control_panel.views
 
 urlpatterns = [
-    path('', jobs.views.home, name='home'),
+    path('', jobs.views.home, name='jobs'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('controlpanel/', control_panel.views.control_panel),
+    path('controlpanel/', control_panel.views.control_panel, name='controlpanel'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
